9756c49e4f22686eeedcfdaeda1a4fbf3dc94fe8
k.shilovskiy <k.shilovskiy@reply.de>
2024-12-08 19:30:53 +0100

    Step4: Add database config

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
-from app.routers import wishlists, auth
+from database import create_tables
+from app.routers import wishlists, auth, users
+
+
+create_tables()
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(wishlists.router, prefix="/wishlists", tags=["wishlists"])
