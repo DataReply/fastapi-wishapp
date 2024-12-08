81a93c0b7378dc2022f4851f04691c752f21dbd5
k.shilovskiy <k.shilovskiy@reply.de>
2024-12-08 19:30:53 +0100

    Step4: Add database config

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel import create_engine, Session, SQLModel
+
+from app.config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.SQL_LITE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+def create_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+DbSession = Annotated[Session, Depends(get_session)]
