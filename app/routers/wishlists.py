c807c9e7e4a923b99e8f5bd07d0ead5fdba27412
k.shilovskiy <k.shilovskiy@reply.de>
2024-12-10 17:56:00 +0100

    Step4: Fix imports

--- app/routers/wishlists.py
+++ app/routers/wishlists.py
@@ -1,9 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
-from sqlmodel import select
 
-from routers.security import User, get_current_user
 from app.database import DbSession
 from app.models.base import PrimaryKeyType
 from app.models.user import User
